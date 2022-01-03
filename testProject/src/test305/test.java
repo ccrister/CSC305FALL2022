c3c3ad5c0510107667d6c3f7ec11dd1b407f02ed
chriscristerna <chriscristerna@localhost>
2022-01-03 13:40:06 -0800

    added a second print statement to main

--- testProject/src/test305/test.java
+++ testProject/src/test305/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello world");
-		System.out.println("Hello again");
+		System.out.println("Hello again world");
 	}
 
 }
